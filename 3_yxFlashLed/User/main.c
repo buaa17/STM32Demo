1ae717d57dbbc4f4325f5f09fd5e4241c0183d0f
yuanxin <441729467@qq.com>
2019-10-13 18:55:17 +0800

    FlashLed

---
+++ 3_yxFlashLed/User/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"
+#include "delay.h"
+
+
+int main(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB,&GPIO_InitStructure);
+    while (1) {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+			  GPIO_SetBits(GPIOB, GPIO_Pin_14);
+        delay_ms(1000);
+        GPIO_SetBits(GPIOB, GPIO_Pin_13);
+			  GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+        delay_ms(1000);
+    }
+}
+
+
+
