23cedbc997bf9083741015035c07e9d3f128bb18
utotao <32936795+utotao@users.noreply.github.com>
2019-10-13 14:45:40 +0800

    modify MODE & add 2_zttFlashLed first

--- 1_water_light_Demo/User/main.c
+++ 1_water_light_Demo/User/main.c
@@ -5,29 +5,22 @@
 int main(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOE,&GPIO_InitStructure);
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	//Ö´ÐÐ³ÌÐò
 	
 	while(1)
 	{
-		GPIO_ResetBits(GPIOE,GPIO_Pin_13);
-		GPIO_SetBits(GPIOE,GPIO_Pin_14);
-		GPIO_SetBits(GPIOE,GPIO_Pin_15);
+		GPIO_ResetBits(GPIOB,GPIO_Pin_13);
+		GPIO_SetBits(GPIOB,GPIO_Pin_14);;
 		delay_ms(1000);
-		GPIO_ResetBits(GPIOE,GPIO_Pin_14);
-		GPIO_SetBits(GPIOE,GPIO_Pin_13);
-		GPIO_SetBits(GPIOE,GPIO_Pin_15);
+		GPIO_ResetBits(GPIOB,GPIO_Pin_14);
+		GPIO_SetBits(GPIOB,GPIO_Pin_13);;
 		delay_ms(1000);
-		GPIO_ResetBits(GPIOE,GPIO_Pin_15);
-		GPIO_SetBits(GPIOE,GPIO_Pin_13);
-		GPIO_SetBits(GPIOE,GPIO_Pin_14);
-		delay_ms(1000);
-		
 	}
 }
 
